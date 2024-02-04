3da56952edbbe9cd443dc0634f9599d4e2372257
BraxtonMedeiros <97545999+BraxtonMedeiros@users.noreply.github.com>
2024-02-03 17:04:29 -0700

    First commit

--- Calendar/CalendarHelper.swift
+++ Calendar/CalendarHelper.swift
@@ -50,6 +50,6 @@ class CalendarHelper
     func weekDay(date: Date) -> Int
     {
         let components = calendar.dateComponents([.weekday], from: date)
-        return components.weekday!; -1
+        return components.weekday! - 1
     }
 }
