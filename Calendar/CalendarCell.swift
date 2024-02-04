1428241b500da8bbd6433ce927eee3be103b0bb3
BraxtonMedeiros <97545999+BraxtonMedeiros@users.noreply.github.com>
2024-02-03 17:01:24 -0700

    First commit

---
+++ Calendar/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  Calendar
+//
+//  Created by Braxton Medeiros on 2/3/24.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
